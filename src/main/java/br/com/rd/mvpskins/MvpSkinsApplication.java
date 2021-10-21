1c5f1d40b9c1a9a8e59368af7b19d8e012301a79
yasminmq <ymonteiroq@gmail.com>
2021-10-20 23:11:57 -0300

    Criação de repositório

---
+++ src/main/java/br/com/rd/mvpskins/MvpSkinsApplication.java
@@ -0,0 +1,13 @@
+package br.com.rd.mvpskins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvpSkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvpSkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rd/mvpskins/MvpSkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rd.mvpskins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvpSkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
