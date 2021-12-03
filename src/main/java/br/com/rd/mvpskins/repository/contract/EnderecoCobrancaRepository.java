a1919186ca89cee98684846a1128e8639137445d
Yasmin Monteiro <mntr.yasmin@gmail.com>
2021-12-03 15:01:24 -0300

    Correção no post e get da tb de endereço de cobrança

--- src/main/java/br/com/rd/mvpskins/repository/contract/EnderecoCobrancaRepository.java
+++ src/main/java/br/com/rd/mvpskins/repository/contract/EnderecoCobrancaRepository.java
@@ -1,11 +1,13 @@
 package br.com.rd.mvpskins.repository.contract;
 
 import br.com.rd.mvpskins.model.entity.EnderecoCobranca;
-import br.com.rd.mvpskins.model.entity.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EnderecoCobrancaRepository extends JpaRepository<EnderecoCobranca, Long> {
+    EnderecoCobranca findByPedidoId(Long id);
 
 }
